fe3686d4264fb6ea33f6ea8f39cd14a79ff6dbf3
kairavb <kairavb8@gmail.com>
2024-12-05 15:03:41 +0530

    post update

--- main.go
+++ main.go
@@ -10,8 +10,21 @@ func main() {
 	})
 
 	app.Post("/", func(c *fiber.Ctx) error {
-		return c.Render("views/index.html", fiber.Map{"url": "answer"})
+		url := c.FormValue("url")
+
+		if url == "" {
+			return c.Render("views/index.html", fiber.Map{"url": "URL cannot be empty"})
+		}
+
+		surl := GenerateShortURL()
+
+		return c.Render("views/index.html", fiber.Map{"url": surl})
 	})
 
 	app.Listen(":5000")
+}
+
+func GenerateShortURL() string {
+	surl := 6
+	return surl
 }
