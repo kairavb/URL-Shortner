bc8c7fe6baa17dac95ad3a09a045287908ab8187
kairavb <kairavb8@gmail.com>
2024-12-05 14:50:06 +0530

    index page

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world")
+		return c.Render("views/index.html", fiber.Map{})
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return c.Render("views/index.html", fiber.Map{"url": "answer"})
 	})
 
 	app.Listen(":5000")
